17ca065e5fc31a3b01ba483c4d9419a25a4ca520
Isaac Vue <Isaac Vue@192.168.0.23>
2020-11-04 21:08:04 -0800

    Added my name

--- CSC131Lab/src/Hello.java
+++ CSC131Lab/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("This line was added by Kyle Giro.");
-		System.out.println("This line was added by First name Last name.");
+		System.out.println("This line was added by Isaac Vue.");
 		System.out.println("This line was added by First name Last name.");
 		System.out.println("This line was added by First name Last name.");
 		System.out.println("This line was added by First name Last name.");
