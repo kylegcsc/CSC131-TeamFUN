6a16936fa6c46d89a2fb8b706c8a3921cd5e2d49
maan202 <71425178+maan202@users.noreply.github.com>
2020-11-05 20:38:38 -0800

    Add name

--- CSC131Lab/src/Hello.java
+++ CSC131Lab/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("This line was added by Kyle Giro.");
 		System.out.println("This line was added by Isaac Vue.");
 		System.out.println("This line was added by Ignacio Marmolejo.");
-		System.out.println("This line was added by Maan Raoof ");
+		System.out.println("This line was added by Maan Raoof.");
 		System.out.println("This line was added by First name Last name.");
 		System.out.println("This line was added by First name Last name.");
 		System.out.println("This line was added by First name Last name.");
