c0f3eeeda76fa740eb3dc158f97a243380b6fcce
maan202 <71425178+maan202@users.noreply.github.com>
2020-11-17 23:08:53 -0800

    added processNum method

--- TeamProject/src/teamfun/Controller.java
+++ TeamProject/src/teamfun/Controller.java
@@ -1,11 +1,17 @@
 package teamfun;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Labeled;
 import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
 public class Controller {
@@ -66,21 +72,33 @@ public class Controller {
 	    
 	    @FXML
 	    public void processNum(ActionEvent event) {
-	    	
+	    	String value = ((Button)event.getSource()).getText();
+	        screenPrmpt.setText(screenPrmpt.getText() + value);
 	    }
 	    
 	    @FXML
 	    public void processOperators(ActionEvent event) {
 	    	
 	    }
+	    
+	    
+	    public void initialize(URL url, ResourceBundle resourceBundle) {
+	        screenPrmpt.textProperty().addListener(new ChangeListener<String>() {
+	            @Override
+	            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+	                if (!newValue.matches("\\d*([\\.]\\d*)?")) {
+	                    screenPrmpt.setText(oldValue);
+	                }
+	            }
+	        });
+	    }
 
 
-    public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        screenPrmpt.setStyle("-fx-text-inner-color: black; -fx-font-size: 32px;");
-		//screenPrmpt.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
-		screenPrmpt.setText("0");
-
-    }
+//    public void initialize() {
+//        String javaVersion = System.getProperty("java.version");
+//        String javafxVersion = System.getProperty("javafx.version");
+//        screenPrmpt.setStyle("-fx-text-inner-color: black; -fx-font-size: 32px;");
+//		//screenPrmpt.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+//		screenPrmpt.setText("0");
+//    }
 }
