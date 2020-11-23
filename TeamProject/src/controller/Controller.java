4b0084d8e0bb1a37f62426836190b076c3ff09a5
Kyle Giro <kylegiro@csus.edu>
2020-11-23 15:55:31 -0800

    Start OperationButton gui component

--- TeamProject/src/controller/Controller.java
+++ TeamProject/src/controller/Controller.java
@@ -2,8 +2,29 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class Controller {
+	
+	@FXML private Button button_0;
+	@FXML private Button button_1;
+	@FXML private Button button_2;
+	@FXML private Button button_3;
+	@FXML private Button button_4;
+	@FXML private Button button_5;
+	@FXML private Button button_6;
+	@FXML private Button button_7;
+	@FXML private Button button_8;
+	@FXML private Button button_9;
+	@FXML private Button button_multiply;
+	@FXML private Button button_divide;
+	@FXML private Button button_add;
+	@FXML private Button button_subtract;
+	@FXML private Button button_decimal;
+	@FXML private Button button_delete;
+	@FXML private Button button_clear;
+	@FXML private Button button_clearentry;
+	@FXML private Button button_evaluate;
 	
 	@FXML private Display display;
 	
---
+++ TeamProject/src/controller/OperationButton.java
@@ -0,0 +1,53 @@
+package controller;
+
+import java.io.IOException;
+
+import application.OperationStrategy;
+import application.OperationStrategyAdd;
+import application.OperationStrategyDivide;
+import application.OperationStrategyMultiply;
+import application.OperationStrategySubtract;
+import javafx.beans.NamedArg;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+
+public class OperationButton extends Button {
+
+	private OperationStrategy operationStrategy;
+	
+	// FXML property set declaratively in the .fxml file
+	private final String operation;		
+	
+	public OperationButton(@NamedArg("operation") String operation) {
+		this.operation = operation;
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/calcbutton.fxml"));
+		loader.setRoot(this);		
+		try {
+			loader.load();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}		
+		this.setText(operation);
+		operationStrategy = operationPropertyToStrategy(operation); 
+	}
+	
+	public OperationStrategy getOperation() {
+		return operationStrategy;
+	}
+	
+	private static final OperationStrategy operationPropertyToStrategy(String operation) {
+		switch(operation) {
+		case "+":
+			return new OperationStrategyAdd();
+		case "-":
+			return new OperationStrategySubtract();
+		case "/":
+			return new OperationStrategyDivide();
+		case "*":
+			return new OperationStrategyMultiply();			
+		}
+		// todo throw an exception if the property in the fxml is not set properly
+		return null;
+	}
+
+}
