4cf61103fdc789a50c7416a31b97e63754954963
edgar153 <edgargranados153@gmail.com>
2020-11-25 16:54:34 -0800

    Added the Negate Feature to the Calculator

---
+++ TeamProject/src/application/OperationStrategyNegate.java
@@ -0,0 +1,20 @@
+package application;
+
+public class OperationStrategyNegate implements OperationStrategy
+{
+
+	@Override
+	public double doOperation(double leftOperand, double rightOperand)
+	{
+		// TODO Auto-generated method stub
+		return -leftOperand;
+	}
+
+	@Override
+	public char getChar()
+	{
+		// TODO Auto-generated method stub
+		return '-';
+	}
+
+}
